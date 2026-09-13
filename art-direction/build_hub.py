42723ea2ce5b2a3140e67880c4d84f96938729de
CaptainPickard <pickard.nicko@gmail.com>
2026-09-13 05:27:55 +0000

    Feat: Armor transition pass - faulds, gorget, full leg harness added as layers; neck/waist/hip gaps sealed; vision QA: fully armored no gaps; hub Layer Lab now 8 live layers

--- art-direction/build_hub.py
+++ art-direction/build_hub.py
@@ -20,7 +20,7 @@ def png_small(path, max_h=460):
 
 lbase = artdir + '/sprites/noble-knight/layers'
 L = {k: png_small(lbase + '/normalized/' + k + '.png') for k in ['body','cloak','chest','helm']}
-LF = {k: png_small(lbase + '/normalized/' + k + '.png') for k in ['body-front','piece-cloak','piece-greaves','piece-gloves','piece-chest','piece-helm']}
+LF = {k: png_small(lbase + '/normalized/' + k + '.png') for k in ['body-front','piece-cloak','piece-legs','piece-faulds','piece-gorget','piece-gloves','piece-chest','piece-helm']}
 
 def fig(name, title, cap):
     return ('<figure class="panel"><div class="frame"><img src="' + b64f(name) + '"></div>'
@@ -80,7 +80,8 @@ html = open(artdir + '/hub-template.html').read()
 html = html.replace('__WORLD__', world).replace('__CAST__', cast).replace('__SCENES__', scenes)
 html = html.replace('__CAMPS__', camps).replace('__PILOTS__', pilots)
 html = html.replace('__L_CLOAK__', LF['piece-cloak']).replace('__L_BODY__', LF['body-front'])
-html = html.replace('__L_GREAVES__', LF['piece-greaves']).replace('__L_GLOVES__', LF['piece-gloves'])
+html = html.replace('__L_LEGS__', LF['piece-legs']).replace('__L_FAULDS__', LF['piece-faulds'])
+html = html.replace('__L_GORGET__', LF['piece-gorget']).replace('__L_GLOVES__', LF['piece-gloves'])
 html = html.replace('__L_CHEST__', LF['piece-chest']).replace('__L_HELM__', LF['piece-helm'])
 html = html.replace('__K_COMPOSITE__', b64f('knight-composite')).replace('__K_CONCEPT__', b64f('n-knight'))
 
